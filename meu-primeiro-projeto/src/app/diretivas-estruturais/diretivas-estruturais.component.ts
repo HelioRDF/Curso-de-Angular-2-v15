d2799f5f415faf78ddaf96e565ed8b7f5d542d76
Helio Franca <heliordf@hotmail.com>
2023-06-26 18:33:57 -0300

    Diretiva SwitchCase

--- meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasEstruturaisComponent implements OnInit {
   public valorIf: boolean = true
   public novoValorIf: boolean = true
+  public valorSwitch:string = "Valor 03"
+  public nome:string = "Luziinha"
+  public idade:number = 60
   public list: Array<{ nome: string, idade: number }> = [
     { nome: "Helio", idade: 34 },
     { nome: "Aline", idade: 30 },
@@ -25,7 +28,7 @@ export class DiretivasEstruturaisComponent implements OnInit {
   }
 
   public onClickAddList() {
-    this.list.push({ nome: "Luzia", idade: 60 })
+    this.list.push({ nome: this.nome, idade: this.idade })
   }
 
   public onClickEventList(event: number) {
