fb8574760a34e17c3a71c216f6f75291b9ac9c43
Helio Franca <heliordf@hotmail.com>
2023-06-26 13:48:34 -0300

    Diretiva For

--- meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
+++ meu-primeiro-projeto/src/app/diretivas-estruturais/diretivas-estruturais.component.ts
@@ -8,14 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class DiretivasEstruturaisComponent implements OnInit {
   public valorIf: boolean = true
   public novoValorIf: boolean = true
+  public list: Array<{ nome: string, idade: number }> = [
+    { nome: "Helio", idade: 34 },
+    { nome: "Aline", idade: 30 },
+    { nome: "Ester", idade: 10 },
+    { nome: "Faby", idade: 34 },
+  ];
   ngOnInit(): void {
     setInterval(() => {
       this.valorIf = !this.valorIf
     }, 3000)
   }
 
-  mudarNovoValorIf(){
-    this.novoValorIf=!this.novoValorIf
+  mudarNovoValorIf() {
+    this.novoValorIf = !this.novoValorIf
+  }
+
+  public onClickAddList() {
+    this.list.push({ nome: "Luzia", idade: 60 })
+  }
+
+  public onClickEventList(event: number) {
+    console.log(event)
+    this.list.splice(event, 1)
   }
 
 }
