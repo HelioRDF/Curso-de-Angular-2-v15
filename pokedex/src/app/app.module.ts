b1ecff5832d4158fe842627614dc68e73bc36314
Helio Franca <heliordf@hotmail.com>
2023-08-14 19:32:16 -0300

    Configurando a buscar por API

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PagesModule } from './pages/pages.module';
 
+import {HttpClientModule} from '@angular/common/http'
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { PagesModule } from './pages/pages.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    PagesModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
